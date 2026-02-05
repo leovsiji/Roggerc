eb1446482af43222d7bd18a935c3c3945a4fd3cb
leovsiji2025-joyhat <leovsiji2025@gmail.com>
2026-02-06 04:47:07 +0530

    win2 term5

--- Roggerc/roggerc/rogger/main.py
+++ Roggerc/roggerc/rogger/main.py
@@ -2,13 +2,12 @@ import os
 from rogger.dec import env
 from rogger.plat import win,term
 
-print("hi")
+
 g=env()
-print(g)
+
 
 def main():
     if g=="termux":
         term.main()
     elif g=="windows":
-        print("hi")
         win.main()
